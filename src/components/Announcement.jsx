6e309a365895900166a6ba0273e7713ec0dc8507
shikubro90 <shikubro90@gmail.com>
2022-07-07 02:27:00 +0600

    started

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 500;
+    font-size: 14px;
+`;
+
+const Announcement = () => {
+    return (
+        <Container>
+            Super deal! Free shipping on order over $50
+        </Container>
+    );
+};
+
+export default Announcement;
