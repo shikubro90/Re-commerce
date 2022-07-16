c858bc22e1ae48024058117c231135301414628c
shikubro90 <shikubro90@gmail.com>
2022-07-17 02:37:13 +0600

    Update

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,7 +3,9 @@ import {categories} from '../data.js'
 import CategoryItem from './CategoryItem';
 
 const Container = styled.div`
-
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
 `;
 
 const Categories = () => {
